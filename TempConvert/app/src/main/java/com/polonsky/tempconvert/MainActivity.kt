918b278387513a043fdc4cf5662e0fec5c8742a3
Polomich <michpolonsky@gmail.com>
2021-10-11 04:18:13 -0400

    finish

--- TempConvert/app/src/main/java/com/polonsky/tempconvert/MainActivity.kt
+++ TempConvert/app/src/main/java/com/polonsky/tempconvert/MainActivity.kt
@@ -9,17 +9,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //create GUI objects
         var tempEditText = findViewById<EditText>(R.id.temp)
         var resultTextView = findViewById<TextView>(R.id.result)
         var weatherImageView = findViewById<ImageView>(R.id.weatherImage)
 
+        //create and set button
         val calculateBtn = findViewById<Button>(R.id.calculate)
         calculateBtn.setOnClickListener{
             var temperature = Temperature(tempEditText.text.toString().toDouble())
+            //if temperature is out of range
             if((temperature.getCelsius() < -50)||(temperature.getCelsius() > 50)){
                 displayError()
             }
             else{
+                //show temperature and image
                 resultTextView.text = temperature.calculate().toString()
                 temperature.showImage(weatherImageView)
             }
--- TempConvert/app/src/main/java/com/polonsky/tempconvert/temperature.kt
+++ TempConvert/app/src/main/java/com/polonsky/tempconvert/temperature.kt
@@ -7,30 +7,36 @@ class Temperature(val tempCelsius: Double){
     private val celsius: Double = tempCelsius
     private var fahren: Double = 0.0
 
+    /*
+    getter for celsius
+     */
     public fun getCelsius():Double{
         return this.celsius
     }
 
+    /*
+    calculate and return temperature in fahrenheit
+     */
     public fun calculate(): Double {
         this.fahren = (this.celsius) * 1.8 + 32
         return this.fahren
     }
 
+    /*
+    show image corresponding to the temperature
+     */
     public fun showImage(image: ImageView){
+        //for warmm weather
         if(this.celsius in 19.0..24.0){
             image.setImageResource(R.drawable.niceweather)
         }
+        //hot weather
         else if(this.celsius > 24){
             image.setImageResource(R.drawable.hotweather)
         }
+        //cold weather
         else{
             image.setImageResource(R.drawable.coldweather)
         }
     }
-    /*
-    6.	When the temperature given is between 19 – 24 inclusive, the App should display an image that
-    represents a nice weather. If the temperature is greater than 24, an image representing hot weather
-     should be displayed. If the temperature is <19, an image indicating cold weather should be displayed.
-     */
-
 }
